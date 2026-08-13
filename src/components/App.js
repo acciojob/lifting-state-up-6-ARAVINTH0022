f301e5c03f17ed2972c5cb4fa9fc43ec1ceb2f33
ARAVINTH0022 <ARAVINTH0022@users.noreply.github.com>
2026-08-13 12:25:42 +0530

    Implement Lifting State Up 6 solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
-import React from "react";
+import React, { useState } from "react";
+import TodoList from "./TodoList";
 import './../styles/App.css';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a React app", completed: false },
+    { id: 3, text: "Deploy the React app", completed: false }
+  ]);
+
+  const handleComplete = (id) => {
+    setTodos(todos.map(todo => 
+      todo.id === id ? { ...todo, completed: true } : todo
+    ));
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const TodoList = ({ todos, handleComplete }) => {
+  return (
+    <div>
+      <h2>Child Component</h2>
+      <ul>
+        {todos && todos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.text}</span>
+            {!todo.completed && (
+              <button onClick={() => handleComplete(todo.id)}>
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
